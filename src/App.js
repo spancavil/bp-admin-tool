8c7f321a7ec25f6e6d690f73431e5aa07f01acc9
ÂMauro069 <maurovera069@gmail.com>
2022-03-15 10:36:40 -0300

    Add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
   return (
-    <div className="App">
-      Battlepalooza admin tool
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
